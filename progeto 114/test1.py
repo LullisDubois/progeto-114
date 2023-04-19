cfe8e51fe0b206c875d1a1a672b5e121f6c25018
LullisDubois <101745628+LullisDubois@users.noreply.github.com>
2023-04-19 16:06:16 -0300

    Add files via upload

---
+++ progeto 114/test1.py
@@ -0,0 +1,9 @@
+a = 33
+b = 33
+print(type(a))
+print(type(b))
+
+if b > a:
+    print("b é maior que a") 
+elif b == a:
+    print("b e a são iguais")
---
+++ progeto 114/test2.py
@@ -0,0 +1,6 @@
+def my_function(fname):
+    print(fname+" Refsnes")
+
+my_function("Emily")
+my_function("Jack")
+my_function("Tony")
